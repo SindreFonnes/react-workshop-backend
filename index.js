c5ceee8fc4d67d92a91bea7a68d9b28de838c96d
Sindre Fonnes <sindre.fonnes@scaleaq.com>
2022-08-26 08:59:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express();
+
+app.use(express.static(join(__dirname, "build")));
+
+const port = 3000;
+
+const apiBasePath = "/api";
+
+app.get(`${apiBasePath}/hello`, (req, res) => {
+    res.send({hello: "world"});
+})
